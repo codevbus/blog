1f1f30046455fa6e6c526f45f74b1324c419e7bf
Mike Vanbuskirk <mvanbus43@gmail.com>
2018-06-08 18:56:28 -0400

    part 2 of travis-ci/serverless article

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Mike Vanbuskirk'
 SITENAME = 'sysengcooking'
-SITEURL = 'http://localhost:8000'
+SITEURL = 'http:/localhost:8000'
 SITETITLE = AUTHOR
 SITESUBTITLE = 'devops recipes'
 SITELOGO = '/images/beach_jpg.jpg'
